3c7fe745cfb374211a9b3ff8d3a6011e195183ae
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-15 12:26:00 -0500

    Added Rand_DDI template

---
+++ Datasets/Rand_DDI.py
@@ -0,0 +1,34 @@
+"""
+Author: Rohan Singh
+Feb 15, 2022
+This python module contains source code to create a randomized Drug-Drug-Interaction Tensor
+"""
+
+# Imports
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import random as rand
+
+#%%
+"""
+This cell contains the code to create an n1xn1xn2 dimension tensor for the DDI
+"""
+
+def create_ddi(seed):
+    pass
+
+
+#%%
+"""
+This cell contains the main method
+"""
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
+
+#%%
