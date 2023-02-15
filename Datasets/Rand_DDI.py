3ca87533382fbfb6dd31afe4f0df3b22468e7360
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-15 12:57:23 -0500

    Added code to make empty tensor

--- Datasets/Rand_DDI.py
+++ Datasets/Rand_DDI.py
@@ -15,8 +15,15 @@ import random as rand
 This cell contains the code to create an n1xn1xn2 dimension tensor for the DDI
 """
 
-def create_ddi(seed):
-    pass
+def create_ddi(n1, n2):
+    tensor = []
+    for i in range (0,n2,1):
+        matrix = []
+        for j in range (0, n1, 1):
+            matrix.append(np.zeros(n1))
+        tensor.append(np.array(matrix))
+    return np.array(tensor)
+
 
 
 #%%
@@ -26,6 +33,8 @@ This cell contains the main method
 
 def main():
     print("Hello World!")
+    ddi = create_ddi(3,5)
+    print(ddi)
 
 
 if __name__ == "__main__":
