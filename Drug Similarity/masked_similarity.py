9a3d358da61454ccc67a550e56af5c46563c30b3
Rohan Singh <rohan.b.singh54@gmail.com>
2023-03-06 20:58:49 -0500

    Added Skeletal Code

---
+++ Drug Similarity/masked_similarity.py
@@ -0,0 +1,19 @@
+"""
+Author: Rohan Singh
+Python Module for finding creating a similarity matrix between two vectors
+"""
+
+#  Imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_similarity(vec1, vec2):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
